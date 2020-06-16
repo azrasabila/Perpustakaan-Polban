a7e3149c4a6a8125ab6104798a80b701d047f0a8
azra sabila <azrasabila@gmail.com>
2020-06-16 15:01:51 +0700

    Update FR03_AZ.java

--- src/test/java/com/id/perpus/book/FR03_AZ.java
+++ src/test/java/com/id/perpus/book/FR03_AZ.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class UntitledTestCase {
+public class FR03_AZ {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
